cc599ea3966b231caed6700f6639e20e96b07274
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2017-11-22 02:45:56 +0100

    fixed logger
    added main method

--- src/main/java/org/opencvapp/MainApp.java
+++ src/main/java/org/opencvapp/MainApp.java
@@ -1,19 +1,42 @@
 package org.opencvapp;
 
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application{
+	
+	private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		launch();
+		BasicConfigurator.configure();
+		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		
+		log.info("App is starting");
+		String fxmlFile = "/views/MainView.fxml";
+		log.debug("Loading FXML for main view from: {}", fxmlFile);
+		FXMLLoader loader = new FXMLLoader();
+		Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+		log.debug("Showing JFX scene");
+		Scene scene = new Scene(rootNode);
+		scene.getStylesheets().add("/styles/styles.css");
+
+		primaryStage.setTitle("OpenCV Application");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 }
