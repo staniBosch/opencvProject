4bec334fb903ac9232ff718f0086fc1c6bf5f4c6
Stanislaus Bosch <stanislaus.bosch@hs-bochum.de>
2017-11-22 12:18:09 +0100

    - added base structure for tests

---
+++ src/test/java/org/opencvapp/GeneralTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.opencvapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author SB
+ *
+ */
+public class GeneralTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		assertTrue(true);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		assertTrue(true);		
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		assertTrue(true);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
